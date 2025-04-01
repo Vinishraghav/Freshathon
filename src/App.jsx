f60133502035935a7e95f96cf0e2905c73cd1bb9
Vinishraghav <vinish523@gmail.com>
2025-04-01 18:42:53 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-primary">Welcome to Freshathon</h1>
+      <p className="lead">Bootstrap is successfully integrated!</p>
+      <button className="btn btn-success">Click Me</button>
+    </div>
+  );
+}
+
+export default App;
